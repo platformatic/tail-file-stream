eeb90ca299d8ab682a26414415e0b09e5cf7aa68
Ivan Tymoshenko <ivan@tymoshenko.me>
2024-03-13 12:11:43 +0100

    Update index.js
    Co-authored-by: Matteo Collina <matteo.collina@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /*
+Copyright Platformatic. All rights reserved.
 Copyright Node.js contributors. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
