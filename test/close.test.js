8dbbe394b0e484c12f614e0dad3e68233015f9fa
Ivan Tymoshenko <ivan@tymoshenko.me>
2024-03-13 15:01:52 +0100

    update close with callback test

--- test/close.test.js
+++ test/close.test.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const assert = require('node:assert')
 const { test } = require('node:test')
 const { join } = require('node:path')
 const { createReadStream } = require('..')
@@ -8,8 +9,8 @@ test('should close stream with a callback', (t, done) => {
   const pathToFile = join(__dirname, 'files', 'test-file-1.txt')
   const stream = createReadStream(pathToFile, { autoWatch: false })
 
-  stream.on('data', () => {})
-  stream.on('end', () => {
-    stream.close(done)
+  stream.close((err) => {
+    assert.strictEqual(err.code, 'ERR_STREAM_PREMATURE_CLOSE')
+    done()
   })
 })
