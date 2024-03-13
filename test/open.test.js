08c9111e22eb9221c1b8f4cc31c485416a68221f
Ivan Tymoshenko <ivan@tymoshenko.me>
2024-03-13 17:26:07 +0100

    chore: add renovate config

--- test/open.test.js
+++ test/open.test.js
@@ -5,28 +5,6 @@ const { test } = require('node:test')
 const { join } = require('node:path')
 const { createReadStream } = require('..')
 
-test('should open a file', (t, done) => {
-  const pathToFile = join(__dirname, 'files', 'test-file-1.txt')
-  const stream = createReadStream(pathToFile, { autoWatch: false })
-
-  let openEmitted = false
-  stream.on('open', (fd) => {
-    openEmitted = true
-    assert.strictEqual(typeof fd, 'number')
-  })
-
-  let readyEmitted = false
-  stream.on('ready', () => {
-    readyEmitted = true
-  })
-
-  setImmediate(() => {
-    assert.strictEqual(openEmitted, true)
-    assert.strictEqual(readyEmitted, true)
-    done()
-  })
-})
-
 test('should fail to open a fail', (t, done) => {
   const pathToFile = join(__dirname, 'files', 'test-file-wrong.txt')
   const stream = createReadStream(pathToFile, { autoWatch: false })
