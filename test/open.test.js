7751d33a40040975aa35ebb7f38c011414466248
Ivan Tymoshenko <ivan@tymoshenko.me>
2024-03-14 10:07:19 +0100

    update

--- test/open.test.js
+++ test/open.test.js
@@ -3,7 +3,28 @@
 const assert = require('node:assert')
 const { test } = require('node:test')
 const { join } = require('node:path')
+const { tspl } = require('@matteo.collina/tspl')
 const { createReadStream } = require('..')
+
+test('should open a file', (t, done) => {
+  const { strictEqual, ok } = tspl(t, { plan: 2 })
+
+  const pathToFile = join(__dirname, 'files', 'test-file-1.txt')
+  const stream = createReadStream(pathToFile, { autoWatch: false })
+
+  stream.on('open', (fd) => {
+    strictEqual(typeof fd, 'number')
+  })
+
+  stream.on('ready', () => {
+    ok(true)
+  })
+
+  stream.on('data', () => {
+    stream.destroy()
+    done()
+  })
+})
 
 test('should fail to open a fail', (t, done) => {
   const pathToFile = join(__dirname, 'files', 'test-file-wrong.txt')
